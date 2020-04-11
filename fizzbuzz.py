9f73b7142157a2fadbfeaaca6985831c0138a9bb
beezee <brian.zeligson@gmail.com>
2020-04-11 15:55:25 -0400

    from single choice to arbitrary choice in inverse

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -15,8 +15,21 @@ def fizzbuzz(nums: List[int]) -> List[str]:
     res.append(s)
   return res
 
-@given(lists(integers(), max_size=10))
-def test_fizzbuzz(nums: List[int]) -> None:
+@given(lists(integers(), max_size=10), integers(min_value=1))
+def test_fizzbuzz(nums: List[int], seed: int) -> None:
   # with isomorphism -> encode(decode(x)) == x
-  # with idempotent -> fizzbuzz(inverse(fizzbuzz(x))) == fizzbuzz(x)
-  assert False
+  # with idempotent -> 
+  #   inverse(fizzbuzz(inverse(fizzbuzz(x)))) == inverse(fizzbuzz(x))
+  def inverse(strs: List[str]) -> List[int]:
+    nums: List[int] = []
+    for s in strs:
+      num = None
+      if s == "Fizz":
+        num = (seed + 1 if seed % 5 == 0 else seed) * 3
+      elif s == "Buzz":
+        num = (seed + 1 if seed % 3 == 0 else seed) * 5
+      elif s == "FizzBuzz":
+        num = seed * 15
+      nums.append(num or int(s))
+    return nums
+  assert inverse(fizzbuzz(inverse(fizzbuzz(nums)))) == inverse(fizzbuzz(nums))
