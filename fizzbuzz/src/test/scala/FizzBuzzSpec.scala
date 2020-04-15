83ad65f9625a9a9c144e801ff5e0ccf5e1a9519b
beezee <brian.zeligson@gmail.com>
2020-04-15 18:54:24 -0400

    handle zero case

--- fizzbuzz/src/test/scala/FizzBuzzSpec.scala
+++ fizzbuzz/src/test/scala/FizzBuzzSpec.scala
@@ -5,10 +5,11 @@ import org.scalacheck.Prop.forAll
 
 object FizzBuzz {
   def apply(nums: List[Int]): List[String] =
-    nums.map(n => (n % 3, n % 5) match {
-      case (0, 0) => "FizzBuzz"
-      case (0, _) => "Fizz"
-      case (_, 0) => "Buzz"
+    nums.map(n => (n, n % 3, n % 5) match {
+      case (0, _, _) => "0"
+      case (_, 0, 0) => "FizzBuzz"
+      case (_, 0, _) => "Fizz"
+      case (_, _, 0) => "Buzz"
       case _ => n.toString
     })
 }
