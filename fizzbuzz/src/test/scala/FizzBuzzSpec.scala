9bcb270dce1addd1d94affa9386de37b12cf4ca2
beezee <brian.zeligson@gmail.com>
2020-04-15 18:18:21 -0400

    second stop

--- fizzbuzz/src/test/scala/FizzBuzzSpec.scala
+++ fizzbuzz/src/test/scala/FizzBuzzSpec.scala
@@ -1,26 +1,38 @@
 package bz.fizzbuzz
 
 import org.scalacheck.Properties
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.{forAll, propBoolean}
 
 object FizzBuzz {
   def apply(nums: List[Int]): List[String] =
-    nums.map(n => (n % 3, n % 5) match {
-      case (0, 0) => "FizzBuzz"
-      case (0, _) => "Fizz"
-      case (_, 0) => "Buzz"
+    nums.map(n => (n, n % 3, n % 5) match {
+      case (0, _, _) => "0"
+      case (_, 0, 0) => "FizzBuzz"
+      case (_, 0, _) => "Fizz"
+      case (_, _, 0) => "Buzz"
       case _ => n.toString
     })
 }
 
 object FizzBuzzSpecification extends Properties("FizzBuzz") {
 
-  property("fizzbuzz") = forAll { (nums: List[Int]) =>
+  property("fizzbuzz") = forAll { (nums: List[Int], _seed: Int) =>
     // with isomorphism -> encode(decode(x)) == x
     // with idempotent -> 
     //   inverse(FizzBuzz(inverse(FizzBuzz(nums)))) == 
     //      inverse(FizzBuzz(nums))
-    false
+    val seed = _seed.max(1).min(Integer.MAX_VALUE / 15)
+    val inverse = (_: List[String]).map(x => (x, seed % 5, seed % 3) match {
+      case ("0", _, _) => 0
+      case ("FizzBuzz", _, _) => 15 * seed
+      case ("Buzz", _, 0) => 5 * (seed + 1)
+      case ("Buzz", _, _) => 5 * seed
+      case ("Fizz", 0, _) => 3 * (seed + 1)
+      case ("Fizz", _, _) => 3 * seed
+      case (s, _, _) => s.toInt
+    })
+    ((FizzBuzz(inverse(FizzBuzz(nums))) == FizzBuzz(nums)) :|
+      s"${FizzBuzz(inverse(FizzBuzz(nums)))} == ${FizzBuzz(nums)}")
   }
 
 }
